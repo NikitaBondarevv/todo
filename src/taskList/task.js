94b9c7417a568fc6ff8b0cfc8361a3abfc7aa68b
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-12-11 21:09:35 +0200

    Move progress class name to function

--- src/taskList/task.js
+++ src/taskList/task.js
@@ -2,9 +2,17 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
+const getProgressClassName = (done) => {
+  if (done) return styles.done
+
+  if (done === undefined) return styles.progress
+
+  return styles.waiting
+}
+
 export const Task = ({ index, done, title, children }) => (
   <li className={done ? styles.done : styles.tasks}>
-    <span className={done ? styles.done : done === undefined ? styles.progress : styles.waiting} />
+    <span className={getProgressClassName()} />
     <p className={done === undefined ? styles.taskInProgress : ''}>{`${index}. ${title}`}</p>
     {children}
   </li>

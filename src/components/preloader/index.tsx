e9edd73ded5a9c9f98a7e227285e3a5f0b9a8fa4
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-01-14 15:42:11 +0200

    Creating a Mailto form

--- src/components/preloader/index.tsx
+++ src/components/preloader/index.tsx
@@ -1,7 +1,5 @@
 import styles from './styles.css'
 
 export const Preloader = () => (
-  <div className={styles.preloader}>
-    <div className={styles.loader}></div>
-  </div>
+  <div className={styles.preloader} />
 )

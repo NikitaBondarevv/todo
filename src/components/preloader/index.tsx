51f5be2d0e527b4d116d62b6f87fedb160a7fb7b
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-01-14 14:21:36 +0200

    Remake preloader

--- src/components/preloader/index.tsx
+++ src/components/preloader/index.tsx
@@ -2,10 +2,6 @@ import styles from './styles.css'
 
 export const Preloader = () => (
   <div className={styles.preloader}>
-    <div className={styles.loadingioSpinner}>
-      <div className={styles.loader}>
-        <div></div>
-      </div>
-    </div>
+    <div className={styles.loader}></div>
   </div>
 )

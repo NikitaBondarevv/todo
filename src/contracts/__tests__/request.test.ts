6cb117940777b9677a9b3887a365f12e7405502c
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-03-27 21:33:48 +0300

    Update tests for request.ts

--- src/contracts/__tests__/request.test.ts
+++ src/contracts/__tests__/request.test.ts
@@ -1,8 +1,18 @@
+import { act } from '@testing-library/react'
+
 import { makeRequest } from 'contracts/request'
 import { request } from 'contracts/request'
 
 const mockData = { data: 'test' }
 const mockResponse = { json: () => mockData } as unknown as Response
+const options = {
+  body: "{\"data\":\"test\"}",
+  credentials: 'include',
+  headers: {
+    "Content-type": 'application/json; charset=utf-8'
+  },
+  method: 'GET'
+}
 
 describe('makeRequest', () => {
   test('should return some value', async () => {
@@ -13,9 +23,18 @@ describe('makeRequest', () => {
 
   test('should throw an error on failed request', async () => {
     const error = 'test error'
+
     jest.spyOn(global, 'fetch').mockRejectedValueOnce(error)
 
     await expect(makeRequest('some')).rejects.toEqual(new Error(error))
+  })
+
+  test('should add for options "header and body" if data passed in argument', async () => {
+    const fakeFetch = jest.spyOn(global, 'fetch').mockResolvedValueOnce(mockResponse)
+
+    await act(async () => await makeRequest('some', mockData))
+
+    expect(fakeFetch).toHaveBeenCalledWith('http://localhost:8086/some', options)
   })
 })
 

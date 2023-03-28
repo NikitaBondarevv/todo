6b839ed9cce03f3a40d07bd604884615f660e7c3
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-03-28 13:46:58 +0300

    Update tests for tabs/index.tsx

--- src/components/tabs/__tests__/index.test.tsx
+++ src/components/tabs/__tests__/index.test.tsx
@@ -1,11 +1,24 @@
 import { render } from '@testing-library/react'
 
 import { Tabs } from '..'
+import styles from './styles.css'
 
 describe('<Tabs />', () => {
   test('should match snapshot', () => {
-    const { asFragment } = render(<Tabs children={[]} loading={false} />)
+    const { asFragment } = render(
+    <Tabs loading={false}>
+      <div>test</div>
+    </Tabs>
+    )
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should render <Preloader /> if loading = true', () => {
+    const { container } = render(<Tabs children={[]} loading={true} />)
+
+    const preloader = container.querySelector(`.${styles.preloader}`)
+
+    expect(preloader).toBeInTheDocument()
   })
 })

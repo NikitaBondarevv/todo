b44197932ee8585126bcc0e2dae9f11c796f2e0b
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-03-28 13:39:07 +0300

    Update tests for header

--- src/components/header/__tests__/index.test.tsx
+++ src/components/header/__tests__/index.test.tsx
@@ -1,10 +1,33 @@
-import { customRender } from '__mocks__/customRender'
+import { render } from '@testing-library/react'
+import configureStore from 'redux-mock-store'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import { Header } from '..'
+import { customRender } from '__mocks__/customRender'
+import styles from './styles.css'
+
+const mockStore = configureStore([])
 
 describe('<Header />', () => {
   test('should match snapshot', () => {
     const { asFragment } = customRender(<Header />)
 
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should render <AuthorizedUser /> if "isAuthenticated" true', () => {
+    const initialState = { user: { isAuthenticated: true } }
+    const store = mockStore(initialState)
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    )
+    const authorizedUserElement = container.querySelector(`.${styles.menu}`)
+
+    expect(authorizedUserElement).toBeInTheDocument()
   })
 })

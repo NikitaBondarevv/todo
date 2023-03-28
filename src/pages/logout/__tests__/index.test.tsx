3c73110dbb95598cb9b46ee487b56c1029f20ed7
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-03-28 20:04:55 +0300

    Add tests for logout

---
+++ src/pages/logout/__tests__/index.test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+import { logout } from 'contracts/logout'
+import { customRender } from '__mocks__/customRender'
+import { Logout } from '..'
+
+jest.mock('contracts/logout', () => ({
+  logout: jest.fn()
+}))
+
+describe('<Logout />', () => {
+  const logoutMock = logout as jest.Mock
+
+  test('should match snapshot', () => {
+    const { asFragment } = customRender(<Logout />)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('should call logout if don`t error', () => {
+    customRender(<Logout />)
+
+    expect(logout).toHaveBeenCalled()
+  })
+
+  test('should show error text if "logout" don`t work', async () => {
+    const mockError = 'something went wrong, please try leater!'
+
+    logoutMock.mockRejectedValueOnce(mockError)
+    customRender(<Logout />)
+
+    expect(await screen.findByText(mockError)).toBeInTheDocument()
+  })
+})

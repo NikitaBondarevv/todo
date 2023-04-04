fa2feea90356e96ccbbc350597afb3905e57135b
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-04-04 21:20:21 +0300

    Update tests for profile

--- src/pages/profile/__tests__/index.test.tsx
+++ src/pages/profile/__tests__/index.test.tsx
@@ -1,13 +1,16 @@
-import { fireEvent } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { customRender } from '__mocks__/customRender'
 
 import { Profile } from '..'
-import styles from 'components/userForm/styles.css'
 import { updateUser } from 'contracts/user'
 import { fakeUser } from '__mocks__/entities/user.mock'
 
 jest.mock('contracts/user', () => ({
   updateUser: jest.fn()
+}))
+
+jest.mock('components/userForm', () => ({
+  UserForm: jest.fn().mockReturnValue('UserForm')
 }))
 
 describe('<Profile />', () => {
@@ -16,30 +19,20 @@ describe('<Profile />', () => {
 
     expect(asFragment()).toMatchSnapshot()
   })
-
-  test('should disable email field', () => {
-    const { container } = customRender(<Profile />)
-    const inputEmail = container.querySelector('[data-test="email"]')
-
-    expect(inputEmail!.getAttribute('disabled')).not.toBe(null)
-  })
-
+  
   test('should call updateUser', () => {
-    const { container } = customRender(<Profile />)
-    const form = container.querySelector(`.${styles.loginForm}`)
-    const inputEmail = container.querySelector('[data-test="email"]')
-    const inputFirstName = container.querySelector('[data-test="firstName"]')
-    const inputLastName = container.querySelector('[data-test="lastName"]')
-    const inputPassword = container.querySelector('[data-test="password"]')
-    const inputPasswordRepeat = container.querySelector('[data-test="passwordRepeat"]')
+    const userModule = jest.requireMock<{ UserForm: jest.Mock }>('components/userForm')
 
-    fireEvent.change(inputEmail!, { target: { value: fakeUser.email } })
-    fireEvent.change(inputFirstName!, { target: { value: fakeUser.firstName } })
-    fireEvent.change(inputLastName!, { target: { value: fakeUser.lastName } })
-    fireEvent.change(inputPassword!, { target: { value: fakeUser.password } })
-    fireEvent.change(inputPasswordRepeat!, { target: { value: fakeUser.passwordRepeat } })
+    userModule.UserForm.mockImplementation(
+      ({ onSubmit }: { onSubmit: (data: unknown) => void }) => <button onClick={() => onSubmit(fakeUser)} />
+    )
+    customRender(
+      <Profile />
+    )
 
-    fireEvent.submit(form!)
+    const button = screen.getByRole('button')
+
+    fireEvent.click(button)
 
     expect(updateUser).toHaveBeenCalledWith(fakeUser)
   })

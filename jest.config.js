9819bdf1b711dbac9b34d2785f1cd2d985e8c667
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2023-04-05 12:54:00 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -17,11 +17,11 @@ module.exports = {
   testEnvironment: 'jsdom',
 
   coverageThreshold: {
-    'global': {
-      'branches': 90,
-      'functions': 90,
-      'lines': 90,
-      'statements': 90,
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
